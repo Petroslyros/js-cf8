3cc4e981486b6a1533afb47339e8b04c5c3e3d76
Petros <petrosluros@gmail.com>
2025-07-24 20:33:33 +0300

    js methods

--- browser/notesapp/main.js
+++ browser/notesapp/main.js
@@ -1,6 +1,6 @@
 const daysGR = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']  //like java ENUM class
 
-const monthGR = ['January', 'February', 'March', 'April','May','June','July','August'
+const monthsGR = ['January', 'February', 'March', 'April','May','June','July','August'
   ,'September','Octomber','November','December']
 
 let notes = []
@@ -26,3 +26,73 @@ window.addEventListener('DOMContentLoaded', function() {
   })
 
 })
+
+function printGrDate() {
+  const currentDate = new Date()
+  const day = currentDate.getDay
+  const date = currentDate.getDate
+  const month = currentDate.getMonth
+  const year = currentDate.getFullYear
+  const hours = currentDate.getHours
+  const minutes = currentDate.getMinutes
+  const seconds = currentDate.getSeconds
+
+  const dayStr = daysGR[day]
+  const monthStr = monthsGR[month]
+
+  const dateStr = `${dayStr}, ${date} ${monthStr} ${year}`
+  const timeStr = `${(hours < 10 ? '0' : '')} ${hours} : ${(minutes < 10 ? '0' : '')} ${minutes} : ${(seconds < 10 ? '0' : '')} ${seconds}`
+}
+
+document.querySelector('#dateTxt').innerHTML = `${dateStr}<br>${timeStr}`
+
+//Controller
+function onInsertHandler(obj) {
+  if (!obj?.note) return
+
+  insertNote(obj)
+  reset()
+}
+
+// Model
+function insertNote(obj) {
+   notes = [...notes, obj]
+   count++
+   renderNotes()
+}
+
+// View (render)
+function renderNotes() {
+    const container = document.querySelector('#notesWrapper')
+
+    container.innerHTML = notes.map(note => `<div id="${'noteTemplate' + note.key}"
+      class="flex justify-between items-center px-2px border-b border-black">
+      <div> 
+          <input type="checkbox" id="${'noteCheck' + note.key}" onclick="strikeThrough(${note.key})" class="w-[25px] h-[25px] mr-[5px]" ${note.softDeleted ? 'checked' : ''}>
+
+          <label id="${'noteTxt + note.key'}" for="${'noteCheck' + note.key}" 
+          class="w-[200px] max-h-[150px] overflow-hidden break-words whitespace-normal
+          text-base ${note.softDeleted ? 'line-through text-gray-500' : ''}">
+          ${note.note}</label>
+      </div>
+      <button type="button" id="${'noteDelBtn' + note.key}" class="w-[35px] border border-black rounded-full" onclick="deleteNote(${note.key})"></button>
+      
+      
+      </div>`).join("")
+
+}
+
+// View
+function strikeThrough(key) {
+  notes = notes.map(note => note.key === key ? {...note, softDeleted : !note.softDeleted} : {...note})
+  renderNotes()
+}
+
+function deleteNote(key) {
+  notes = notes.filter(note => note.key !== key)
+  renderNotes()
+}
+
+function reset() {
+  document.querySelector('$inputeNote').value = ''
+}
