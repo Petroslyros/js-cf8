9e4eb323f89fa792eeda8871d945649b4f2c1bc7
Petros <petrosluros@gmail.com>
2025-07-24 19:25:46 +0300

    main.js

--- browser/notesapp/main.js
+++ browser/notesapp/main.js
@@ -6,6 +6,23 @@ const monthGR = ['January', 'February', 'March', 'April','May','June','July','Au
 let notes = []
 let count = 0
 
-window.addEventListener('DOMContentLoaded'. function() {
-  
+window.addEventListener('DOMContentLoaded', function() {
+
+  this.setInterval(() => printGrDate(), 1000)
+
+  this.document.querySelector('#addNoteBtn').addEventListener('click', function() {
+    onInsertHandler(
+      {key: count +1, 
+      note: document.querySelector('#inputNote').value.trim(),
+      softDeleted: false})
+  })
+
+  this.document.querySelector('#inputNote').addEventListener('keyup', function(e) {
+      if(e.key === 'Enter') { onInsertHandler(
+      {key: count +1, 
+      note: e.target.value.trim(),
+      softDeleted: false})
+      }
+  })
+
 })
