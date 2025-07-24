7bd0232781fb6002c0f277a07c1ee3d248869487
Petros <petrosluros@gmail.com>
2025-07-24 19:13:05 +0300

    notesapp

---
+++ browser/notesapp/main.js
@@ -0,0 +1,11 @@
+const daysGR = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']  //like java ENUM class
+
+const monthGR = ['January', 'February', 'March', 'April','May','June','July','August'
+  ,'September','Octomber','November','December']
+
+let notes = []
+let count = 0
+
+window.addEventListener('DOMContentLoaded'. function() {
+  
+})
