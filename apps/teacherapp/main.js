cf3cde4b80ef6ce4025c5851654ad4870a3615e9
Petros <petrosluros@gmail.com>
2025-09-12 19:26:40 +0300

    moves and teachers apps

---
+++ apps/teacherapp/main.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+  fetchCities()
+  document.getElementById('teacherForm').addEventListener('submit', handleFormSubmit)
+})
+
+const fetchCities = async () => {
+  try {
+    const response = await axios.get('./cities.json') //instead of https://api.etc
+    const cities = response.data
+
+    document.getElementById('city').innerHTML = cities
+                                                      .sort((a, b) => a.name.localeCompare(b.name, "el-GR"))
+                                                      .map(city => `<option value="${city.id}">${city.name}</option>`)
+                                                      .join('')
+  } catch (error) {
+    console.error('Error fetching cities')
+  } 
+}
+
+const handleFormSubmit = async (event) => {
+  event.preventDefault() 
+
+  const payload = {
+    firstname: document.getElementById('firstname').value.trim(),
+    lastname: document.getElementById('lastname').value.trim(),
+    vat: document.getElementById('vat').value.trim(),
+    cityId: document.getElementById('city').value.trim()
+  }
+  try {
+      // const response = await axios.post('https://api-aueb.gr', payload)
+      alert('Teacher data submitted' + JSON.stringify(payload))
+  } catch (error) {
+    console.error('Error in submitting the form')
+  }
+}
